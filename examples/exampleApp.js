13bbb97e1d34b083a84bc5e3f22b01d7be219083
Dan Crews <crewsd@gmail.com>
2016-04-12 17:08:12 -0600

    Updating to 1.5.0

--- examples/exampleApp.js
+++ examples/exampleApp.js
@@ -12,6 +12,11 @@
       {
         'and': [
           {
+            'term': {
+              'test.date': '2016-04-08T10:44:06'
+            }
+          },
+          {
             'range': {
               'test.number': {
                 'gte': 650
@@ -50,14 +55,24 @@
         'exists': {
           'field': 'test.term'
         }
-      }
+      },
+      {
+        'range': {
+          'test.otherdate': {
+            'gte': 'now',
+            'lte': 'now+7d'
+          }
+        }
+      },
     ];
 
     data.fields = {
       'test.number': { type: 'number', minimum: 650 },
       'test.term': { type: 'term' },
       'test.boolean': { type: 'term', subType: 'boolean' },
-      'test.state.multi': { type: 'multi', choices: [ 'AZ', 'CA', 'CT' ]}
+      'test.state.multi': { type: 'multi', choices: [ 'AZ', 'CA', 'CT' ]},
+      'test.date': { type: 'date' },
+      'test.otherdate': { type: 'date' },
     };
 
     data.needsUpdate = true;
